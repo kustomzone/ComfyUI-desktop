2f0c5dd6d6e0d1a672e7d150fcfa553c817acb16
Kendal Cormany <kendal.cormany@nexus.io>
2024-10-13 19:02:18 -0700

    increase timer

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  timeout: 2 * 60 * 1000,
+  timeout: 5 * 60 * 1000,
   testMatch: 'src/__tests__/e2e/*.test.ts',
 });
--- src/__tests__/e2e/startup.test.ts
+++ src/__tests__/e2e/startup.test.ts
@@ -33,7 +33,7 @@ test('Load Window', async () => {
 
 test('Wait For Python Server', async () => {
   await activeWindow.waitForTimeout(5000);
-  await expect(activeWindow.locator('h1', {hasText:'ComfyUI'})).toBeEnabled({timeout:120000});
+  await expect(activeWindow.locator('h1', {hasText:'ComfyUI'})).toBeEnabled({timeout:360000});
 });
 
 test('Close App', async () => {
