e58a8b80cd39eb6f7dedafe20255aff76763257d
Kendal Cormany <kendal.cormany@nexus.io>
2024-10-15 19:45:09 -0700

    increase max timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  timeout: 5 * 60 * 1000,
+  timeout: 10 * 60 * 1000,
   testMatch: 'src/__tests__/e2e/*.test.ts',
 });
