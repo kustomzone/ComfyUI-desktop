2a873ab9445eadbb46971836b2c6d53ff79f837a
Kendal Cormany <kendal.cormany@nexus.io>
2024-10-31 12:20:56 -0700

    Fixed up windows make and has nvidia switch

--- scripts/preMake.js
+++ scripts/preMake.js
@@ -6,24 +6,33 @@ const fs = require('fs-extra');
 
 module.exports = async ({ appOutDir, packager, outDir }) => {
 
- 
     const firstInstallOnToDesktopServers =
-    process.env.TODESKTOP_CI && process.env.TODESKTOP_INITIAL_INSTALL_PHASE;
+        process.env.TODESKTOP_CI && process.env.TODESKTOP_INITIAL_INSTALL_PHASE;
     // Do NOT run on CI
     if (process.env.CI || firstInstallOnToDesktopServers) return;
 
-    // Right now only for mac
+    const isNvidia = process.argv[process.argv.length-1] === '--nvidia';
 
-    if (os.platform() !== 'darwin') return;
+    console.log(`<BUILDING COMFYCLI ON ${os.platform()} ${isNvidia && "Nvidia Ver"}>`)
 
-    // If there this file already lets assume comfy has already been installed
-    if (fs.existsSync('./assets/python.tgz')) return; 
-    
-    spawnSync('yarn run make:assets:macos', [''], {shell:true,stdio:"inherit"});
-    spawnSync('cd assets && comfy standalone --rehydrate', [''], {shell:true,stdio:"inherit"});
-    spawnSync('cd assets && mkdir python2/ && tar -xzf python.tgz -C python2/ && rm python.tgz', [''], {shell:true,stdio:"inherit"});
-    spawnSync('cd assets && mv python python3 && mv python2/python python && mkdir output && rm -rf python2 && rm -rf python3', [''], {shell:true,stdio:"inherit"});
-    spawnSync('cd assets/python && filelist=("lib/libpython3.12.dylib" "lib/python3.12/lib-dynload/_crypt.cpython-312-darwin.so" "bin/uv" "bin/uvx" "bin/python3.12") && for file in ${filelist[@]}; do mkdir -p `dirname ../output/$file` && mv "$file" ../output/"$file"; done', [''], {shell:true,stdio:"inherit"});
-    spawnSync('cd assets && tar -czf python.tgz python/ && rm -rf python', [''], {shell:true,stdio:"inherit"});
+    // If this folder is here assume comfy has already been installed
+    if (fs.existsSync('./assets/ComfyUI'))
+    {
+        console.log(">COMFYUI ALREADY BUILT<");
+        return
+    }
+
+    if (os.platform() === 'darwin') {
+        spawnSync('yarn run make:assets:macos', [''], { shell: true, stdio: "inherit" });
+    }
+
+    if (os.platform() === 'win32')
+    {
+        spawnSync('py -m pip install comfy-cli', [''], { shell: true, stdio:['ignore','ignore','inherit'] });
+        const result = spawnSync(`python -c "import os,sysconfig;print(sysconfig.get_path(""scripts"",f""{os.name}_user""))"`, [''], { shell: true, stdio: "pipe" }).stdout.toString();
+        const localPythonModulePath = `PATH=${result.replaceAll('\\','\\\\').trim()};%PATH%`;
+        spawnSync(`set ${localPythonModulePath} && yarn run make:assets:${isNvidia ? 'nvidia' : 'cpu'}`, [''], { shell: true, stdio: "inherit" });
+
+    }
     console.log(">PREMAKE FINISH<");
 }
